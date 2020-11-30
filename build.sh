eb271d07ac0867dc1d3bd1738621b5b38a9d818b
YUJI <mail@yuji.ne.jp>
2020-11-30 20:14:12 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 rm -f list.txt
 touch list.txt
-echo -e "! Title: samurai-filter
-! Homepage: https://github.com/yujixr/samurai-filter.git
-!
-! Licence: 
-! This file is only permitted for personal use under copyright law.
-! Commercial use or  redistribution are prohibited except as permitted by copyright law.
-!
-! Last updated: $(date "+%Y-%m-%d")\n" >> list.txt
+echo -e "[Adblock Plus 2.0]
+! Title: samurai-filter
+! Updated: $(date "+%Y-%m-%d")
+! Expires: 5 days
+! Licence: https://creativecommons.org/licenses/by/3.0/
+! Homepage: https://github.com/yujixr/samurai-filter.git\n" >> list.txt
 for url in $(cat target.txt); do
     echo -e "duckduckgo.com##div:-abp-has(> div > h2 > a[href^=\"$url\"])
 www.google.*##div:-abp-has(> div > div > a[href^=\"$url\"])
