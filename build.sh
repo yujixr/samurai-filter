5d3b0439aa9c899f44a194d2a9da553c2944c1e4
YUJI <mail@yuji.ne.jp>
2020-11-30 20:10:19 +0900

    Fix bug

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ echo -e "! Title: samurai-filter
 ! This file is only permitted for personal use under copyright law.
 ! Commercial use or  redistribution are prohibited except as permitted by copyright law.
 !
-! Last updated: $(date "+%Y-%m-%d")\n"
+! Last updated: $(date "+%Y-%m-%d")\n" >> list.txt
 for url in $(cat target.txt); do
     echo -e "duckduckgo.com##div:-abp-has(> div > h2 > a[href^=\"$url\"])
 www.google.*##div:-abp-has(> div > div > a[href^=\"$url\"])
 www.google.*##div:-abp-has( > g-inner-card a[href^=\"$url\"])
-www.bing.com##li:-abp-has(> h2 > a[href^=\"$url\"])" >>list.txt
+www.bing.com##li:-abp-has(> h2 > a[href^=\"$url\"])" >> list.txt
 done
