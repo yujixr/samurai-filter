ff3a4fa75b208d05f5400da5b165219bd83583d5
YUJI <mail@yuji.ne.jp>
2020-11-30 20:07:52 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 rm -f list.txt
 touch list.txt
+echo -e "! Title: samurai-filter
+! Homepage: https://github.com/yujixr/samurai-filter.git
+!
+! Licence: 
+! This file is only permitted for personal use under copyright law.
+! Commercial use or  redistribution are prohibited except as permitted by copyright law.
+!
+! Last updated: $(date "+%Y-%m-%d")\n"
 for url in $(cat target.txt); do
     echo -e "duckduckgo.com##div:-abp-has(> div > h2 > a[href^=\"$url\"])
 www.google.*##div:-abp-has(> div > div > a[href^=\"$url\"])
